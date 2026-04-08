5bfc968ee41d8f3d49fb524c16d26740f6b579cc
Ben Do <bdo.appworkshop@gmail.com>
2026-04-08 09:04:10 -0500

    Implemented calculateBodyFat method using the Navy Seal formula.

--- src/Model/User.java
+++ src/Model/User.java
@@ -16,7 +16,20 @@ public class User {
 	
 	
 	// method for calculating Body Fat Percentage
-	
+	public double calculateBodyFat() {
+		double bodyFat;
+		
+		if (gender == "male") {
+			bodyFat = 86.010 * Math.log10(waist - neck)
+		               - 70.041 * Math.log10(height)
+		               + 36.76;
+		} else {
+			bodyFat = 163.205 * Math.log10(waist + hip - neck)
+            - 97.684 * Math.log10(height)
+            - 78.387;
+		}
+		return bodyFat;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
