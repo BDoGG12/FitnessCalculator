d8ff0d2fef87a279fc0b99c5e4c62d36f41dbf57
Ben Do <bdo.appworkshop@gmail.com>
2026-04-08 08:52:13 -0500

    Inserted data member gender, implemented calculateBMI method.

--- src/Model/User.java
+++ src/Model/User.java
@@ -4,12 +4,19 @@ public class User {
 	
 	int age;
 	double height, weight, waist, hip, neck;
+	String gender;
 	
 	
 	// method for calculating BMI
+	public double calculateBMI() {
+		double bmi = (weight / height) * 703;
+		
+		return bmi;
+	}
 	
 	
-	// mmethod for calculating Body Fat Percentage
+	// method for calculating Body Fat Percentage
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
