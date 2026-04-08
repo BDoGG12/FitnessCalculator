27ec175623ccfa6680a9445ac8baaafea35d3ca9
Ben Do <bdo.appworkshop@gmail.com>
2026-04-08 08:38:09 -0500

    Created User data model.

---
+++ src/Model/User.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class User {
+	
+	int age;
+	double height, weight, waist, hip, neck;
+	
+	
+	// method for calculating BMI
+	
+	
+	// mmethod for calculating Body Fat Percentage
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
