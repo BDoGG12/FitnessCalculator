977010e0fa680fb7c83e6a1dd232e09b15053e44
Ben Do <bdo.appworkshop@gmail.com>
2026-04-14 13:24:33 -0500

    Added content pane to the gui to display the method selection view.

--- src/FitnessCalculator.java
+++ src/FitnessCalculator.java
@@ -1,11 +1,40 @@
 import javax.swing.*;
+
+import View.MethodSelectView;
+
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class FitnessCalculator extends JFrame {
+	
+	private MethodSelectView methodSelectView;
+	private CardLayout cardLayout;
+	private JPanel cardPanel;
+	
+	public FitnessCalculator() {
+		setPreferredSize(new Dimension(1000, 1000));
+		setMaximumSize(new Dimension(1000, 1000));
+		
+		setMinimumSize(new Dimension(1000, 1000));
+		setResizable(false);
+		setLocationByPlatform(true);
+		
+		
+		
+		Container cp = getContentPane();
+		JPanel panel = new JPanel();
+		methodSelectView = new MethodSelectView();
+		panel.add(methodSelectView);
+		cp.add(panel);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		new FitnessCalculator();
 
 	}
 
